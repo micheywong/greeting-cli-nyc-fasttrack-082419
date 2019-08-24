d27bf202acb6eae35193361cc906f7607498875e
Michelle Wong <mwong@conductor.com>
2019-08-24 18:28:54 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,7 +3,7 @@ def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
 
-puts "Hello. It's nice to meet you."
+  puts "Hello. It's nice to meet you."
 puts "Hello #{name}. It's nice to meet you."
 name = gets.strip
 greeting(name)
